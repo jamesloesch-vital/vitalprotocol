aef9cbfc370d27030f837b87573923450fd77959
Stephen Kelly <srkelly@entouch.net>
2024-07-17 21:23:27 -0500

    component utils

--- app/root.tsx
+++ app/root.tsx
@@ -110,7 +110,7 @@ const App = () => {
 
   return (
     <QueryClientProvider client={client}>
-      <html lang="en">
+      <html lang="en" className="bg-primary">
         <head>
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,11 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./packages/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        primary: "#EFEFEC",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
